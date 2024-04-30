d6960f70f825382437fc2e35e3a1ffbb6ee84f06
Emin1904 <emin.kosovac@elev.ga.ntig.se>
2024-04-30 10:31:18 +0200

    lagt till variabler och svårighetsgrader på spelet

--- prog1/mkdir/flappyspel/flappy-bird.py
+++ prog1/mkdir/flappyspel/flappy-bird.py
@@ -3,3 +3,18 @@ import sys
 import random
 
 pygame.init()
+
+SKÄRM_BREDD = 400
+SKÄRM_HÖJD = 600
+VIT = (255, 255, 255)
+SVART = (0, 0, 0)
+GRÖN = (0, 255, 0)
+FPS = 60
+
+skärm = pygame.display.set_mode((SKÄRM_BREDD, SKÄRM_HÖJD))
+pygame.display.set_caption("Flappy Bird")
+
+font = pygame.font.Font(None, 36)
+enkel_text = font.render("Enkel", True, SVART)
+medel_text = font.render("Medel", True, SVART)
+svår_text = font.render("Svår", True, SVART)
